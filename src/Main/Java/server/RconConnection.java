3f5255a114c5697c310b27fa2482a3549b7e582a
GROSZEK-DEV <kamil.branicki@student.kdg.be>
2025-11-18 18:26:04 +0100

    Implement Closeable in RconConnection
    Refactor RconConnection to implement Closeable interface and manage socket connections.

--- src/Main/Java/server/RconConnection.java
+++ src/Main/Java/server/RconConnection.java
@@ -1,6 +1,47 @@
 package Main.Java.server;
 
-public class RconConnection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
-    //yokoso
+public class RconConnection implements Closeable {
+
+    private final String host;
+    private final int port;
+    private final String password;
+
+    private Socket socket;
+
+    public RconConnection(String host, int port, String password) {
+        this.host = host;
+        this.port = port;
+        this.password = password;
+    }
+
+    public synchronized void connect() throws IOException {
+        if (socket != null && socket.isConnected() && !socket.isClosed()) {
+            return;
+        }
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(host, port), 3000);
+        // TODO: implement RCON handshake / auth packet using 'password'
+    }
+
+    public synchronized boolean isConnected() {
+        return socket != null && socket.isConnected() && !socket.isClosed();
+    }
+
+    public synchronized void disconnect() throws IOException {
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        disconnect();
+    }
+
+    // TODO: add sendCommand(String cmd) that sends RCON packets and returns the response
 }
